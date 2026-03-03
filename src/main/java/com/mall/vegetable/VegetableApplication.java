b9c8f3b9b6246721194593b20326d5f0a6bce8a9
季佳昊 <2014309847@qq.com>
2026-03-03 22:04:49 +0800

    mybatis逆向

--- src/main/java/com/mall/vegetable/VegetableApplication.java
+++ src/main/java/com/mall/vegetable/VegetableApplication.java
@@ -1,9 +1,11 @@
 package com.mall.vegetable;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mall.vegetable.dao")
 public class VegetableApplication {
 
     public static void main(String[] args) {
